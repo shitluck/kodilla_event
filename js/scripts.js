97921ffac6a08094e9ca1cc55d8d55bc1003eb08
Mac Ryndziewicz <fuji.two6@gmail.com>
2017-04-10 12:41:04 +0200

    add files

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+﻿var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+var number = document.getElementsByTagName('li');
+
+console.log(list);
+console.log(add);
+console.log(number);
+
+add.addEventListener('click', function(e) {
+    list.innerHTML += '<li>item ' + number.length + '</li>';
+});
